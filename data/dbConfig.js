e30453e1cd079f727bb2a8769587866fe383914a
Shun Chiang <shunchiang@Shuns-MacBook-Pro.local>
2020-04-26 13:42:31 -0500

    test

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,7 +1,5 @@
-const environment = process.env.NODE_ENV || "development";
-const config = require("../knexfile.js");
-const environmentConfig = config[environment];
 const knex = require("knex");
-const connection = knex(environmentConfig);
+const config = require("../knexfile");
+const environment = process.env.NODE_ENV || "development";
 
-module.exports = connection;
+module.exports = knex(config[environment]);
