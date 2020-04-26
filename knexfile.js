738ed7ffda69b49b88dbd195ab9579d982f1599a
Shun Chiang <shunchiang@Shuns-MacBook-Pro.local>
2020-04-26 12:38:57 -0500

    config

--- knexfile.js
+++ knexfile.js
@@ -11,5 +11,9 @@ module.exports = {
   production: {
     client: "pg",
     connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: "./data/migrations",
+    },
+    seeds: { directory: "./data/seeds" },
   },
 };
